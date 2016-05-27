8dc8178a1b3d0077640de1608875d50580524ca7
cyrilpanicker <cyrilpanicker@gmail.com>
2016-05-27 15:21:21 +0530

    added changes

---
+++ source/demo/demo.tsx
@@ -0,0 +1,9 @@
+import * as React  from 'react';
+
+import './demo-style';
+
+export class DemoComponent extends React.Component<any,any> {
+    render(){
+        return <span>Demo Component</span>
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry:{
+        'main':'./build/main',
+        'libs':[
+            'jquery',
+            'react',
+            'react-dom'
+        ]
+    },
+    output:{
+        path:'./bundled-build',
+        filename:'app.js'
+    },
+    devtool:[
+        'source-map'
+    ],
+    resolve:{
+        extensions:['','.js','.css']
+    },
+    module:{
+        loaders:[
+            {test:/\.css$/,loader:'style-loader!css-loader'}
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template:'./build/index.html'
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name:'libs',
+            filename:'libs.js'
+        })
+    ]
+};
